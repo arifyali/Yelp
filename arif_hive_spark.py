4efe1f82913dd80dc1214f6c1e6fd5b01efe6cd7
Arif Ali <arifyuali@gmail.com>
2016-04-29 19:02:39 +0000

    Splits Data into training and test data (untested)

--- arif_hive_spark.py
+++ arif_hive_spark.py
@@ -31,9 +31,13 @@ features = df.select(df['pricerange'], df['2016_01'], df['2016_02'], df['male_ag
           df['vacant_housing_units'], df['median_housing_value'], df['median_rent'],
           df['success_metric'], df['population'])
 
-feats_list = features.collect()
-feats_dict = [i.asDict() for i in feats_list]
+training, test = features.randomSplit([0.7, 0.3], seed=11L)
 
+feats_train = training.collect()
+train_dict = [i.asDict() for i in feats_train]
+
+feats_test = test.collect()
+test_dict = [i.asDict() for i in feats_test]
 
 def parsePoint(d): ## wont be able to use line.split here?
     d_copy = deepcopy(d) # I hate using deepcopy so much
@@ -42,22 +46,31 @@ def parsePoint(d): ## wont be able to use line.split here?
     values = [float(x) for x in d.values()] ##this block is unusable until we have our Hive Data
     return (pred, Vectors.dense(values))
 
-parsedData = sc.parallelize(map(parsePoint, feats_dict))
+trainParsed = sc.parallelize(map(parsePoint, train_dict))
+testParsed = sc.parallelize(map(parsePoint, test_dict))
 
 ## create training data from this
+
 ##Create test data
 
 ## create validation set
 
 
 # Training error
-df = sqlContext.createDataFrame(parsedData, ["label", "features"])
+trainDf = sqlContext.createDataFrame(trainParsed, ["label", "features"])
+testDf = sqlContext.createDataFrame(testParsed, ["label", "features"])
 lm_model = LinearRegression(featuresCol="features", predictionCol="prediction", maxIter=100, regParam=0.0, elasticNetParam=0.0, tol=1e-6)
-lm_model_fit = lm_model.fit(df)
-lm_transform = lm_model_fit.transform(df)
+lm_model_fit = lm_model.fit(trainDf)
+lm_transform = lm_model_fit.transform(trainDf)
 results = lm_transform.select(lm_transform['prediction'], lm_transform['label'])
 MSE = results.map(lambda(p,l):(p-l)**2).reduce(lambda x,y:x+y)/results.count()
-print("Linear Regression Mean Squared Error = " + str(MSE))
+print("Linear Regression training Mean Squared Error = " + str(MSE))
+
+lm_transform = lm_model_fit.transform(testDf)
+results = lm_transform.select(lm_transform['prediction'], lm_transform['label'])
+MSE = results.map(lambda(p,l):(p-l)**2).reduce(lambda x,y:x+y)/results.count()
+print("Linear Regression testing Mean Squared Error = " + str(MSE))
+
 
 lm_model.save(sc, "LinerRegressionModel")
 
@@ -65,11 +78,16 @@ lm_model.save(sc, "LinerRegressionModel")
 
 #TODO: do same as 54-56 with test and validation
 lasso_model = LinearRegression(featuresCol="features", predictionCol="prediction", maxIter=100, regParam=1.0, elasticNetParam=0.0, tol=1e-6)
-lasso_model_fit = lasso_model.fit(df)
-lasso_transform = lasso_model_fit.transform(df) #change to a test model
+lasso_model_fit = lasso_model.fit(trainDf)
+lasso_transform = lasso_model_fit.transform(trainDf) #change to a test model
 lasso_results = lasso_transform.select(lasso_transform['prediction'], lasso_transform['label'])
 lasso_MSE = lasso_results.map(lambda(p,l):(p-l)**2).reduce(lambda x,y:x+y)/results.count()
-print("LASSO Mean Squared Error = " + str(lasso_MSE))
+print("LASSO training Mean Squared Error = " + str(lasso_MSE))
+
+lasso_transform = lasso_model_fit.transform(testDf) #change to a test model
+lasso_results = lasso_transform.select(lasso_transform['prediction'], lasso_transform['label'])
+lasso_MSE = lasso_results.map(lambda(p,l):(p-l)**2).reduce(lambda x,y:x+y)/results.count()
+print("LASSO training Mean Squared Error = " + str(lasso_MSE))
 
 model.save(sc, "LASSOModel")
 
