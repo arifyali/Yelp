0a9c5ce2734f229e3783093c348c7cddfdc863e7
Jordan Bramble <jordanbramble@gmail.com>
2016-04-29 01:01:22 -0400

    trains model; adds cv; train and test split

--- jordan_hive_spark.py
+++ jordan_hive_spark.py
@@ -1,13 +1,26 @@
 from pyspark.sql import HiveContext
+
 from pyspark.mllib.classification import SVMWithSGD, SVMModel, LogisticRegressionWithSGD
+
 from pyspark.mllib.regression import LabeledPoint
 from pyspark.sql.functions import col, sum
+
+from pyspark.mllib.evaluation import BinaryClassificationMetrics
+from pyspark.mllib.evaluation import MulticlassMetrics
 
 from copy import deepcopy
 
 sc = SparkContext()
 sqlContext = HiveContext(sc)
-qry = "SELECT * FROM census_rest_success"
+qry = """SELECT *,white/population as white_percent,
+         black/population as black_percent,
+         asian/population as asian_percent,
+         pacific_islander/population as pi_percent,
+         other_race/population as other_race_percent,
+         multiple_race/population as multiple_percent,
+         hispanic/population as hispanic_percent
+         FROM census_rest_success where days_open > 365"""
+
 df = sqlContext.sql(qry)
 
 ## Lets train a Support Vector Classifier on this data
@@ -21,41 +34,54 @@ df.agg(*exprs).show()
 
 df = df.dropna()
 
-#TODO: Create Validation set, then create randomized train - test sets, map processing on all
-
 features = df.select(df['pricerange'], df['2016_01'], df['2016_02'], df['male_age_25_29'],
-          df['female_age_25_29'], df['white'], df['black'], df['asian'],
-          df['pacific_islander'], df['other_race'], df['multiple_race'],
-          df['hispanic'], df['median_household_income'], df['median_family_income'],
+          df['female_age_25_29'], df['white_percent'], df['black_percent'], df['asian_percent'],
+          df['pi_percent'], df['other_race_percent'], df['multiple_percent'],
+          df['hispanic_percent'], df['median_household_income'], df['median_family_income'],
           df['vacant_housing_units'], df['median_housing_value'], df['median_rent'],
           df['success_class'], df['population'])
 
-feats_list = features.collect()
-feats_dict = [i.asDict() for i in feats_list]
+training, test = features.randomSplit([0.7, 0.3], seed=11L)
 
+feats_train = training.collect()
+train_dict = [i.asDict() for i in feats_train]
 
-def parsePoint(d): ## wont be able to use line.split here?
+feats_test = test.collect()
+test_dict = [i.asDict() for i in feats_test]
+
+def parsePoint(d):
     d_copy = deepcopy(d) # I hate using deepcopy so much
     pred = d_copy['success_class']
     d.pop('success_class', None)
-    values = [float(x) for x in d.values()] ##this block is unusable until we have our Hive Data
-    return LabeledPoint(pred, values)
+    values = [float(x) for x in d.values()]
+    return LabeledPoint(pred, map(float,values))
 
-parsedData = sc.parallelize(map(parsePoint, feats_dict))
+trainParsed = sc.parallelize(map(parsePoint, train_dict))
+testParsed = sc.parallelize(map(parsePoint, test_dict))
 
-## create training data from this
-##Create test data
+model = SVMWithSGD.train(trainParsed, iterations=100)
 
-## create validation set
-
-model = SVMWithSGD.train(parsedData, iterations=100)
-
-# Training error
-labelsAndPreds = parsedData.map(lambda p: (p.label, model.predict(p.features)))
-trainErr = labelsAndPreds.filter(lambda (v, p): v != p).count()/float(parsedData.count())
+# Training Error
+trainLabelsAndPreds = trainParsed.map(lambda p: (p.label, float(model.predict(p.features))))
+trainErr = trainLabelsAndPreds.filter(lambda (v, p): v != p).count()/float(trainParsed.count())
 print trainErr
 
-#TODO: do same as 54-56 with test and validation
+# Test Error
+testLabelsAndPreds = testParsed.map(lambda p: (p.label, float(model.predict(p.features))))
+testErr = testLabelsAndPreds.filter(lambda (v, p): v != p).count()/float(testParsed.count())
+print testErr
+
+metrics = BinaryClassificationMetrics(testLabelsAndPreds)
+
+print metrics.areaUnderROC
+print metrics.areaUnderPR
+
+mcMetrics = MulticlassMetrics(testLabelsAndPreds)
+
+#TODO: Do this for classes 1.0,0.0 and not just overall
+print mcMetrics.precision()
+print mcMetrics.recall()
+print mcMetrics.fMeasure()
 
 model.save(sc, "SVMModel")
 
