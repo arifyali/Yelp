6e33869285778b11ae8a7168210dc65b654ec449
Jordan Bramble <jordanbramble@gmail.com>
2016-04-30 20:38:36 -0400

    better viz

--- scatter_plot.py
+++ scatter_plot.py
@@ -10,10 +10,10 @@ source = ColumnDataSource(data=df)
 p = Figure(title='Success Metric vs Housing Costs',
            x_axis_label='$/sq. ft.',
            y_axis_label='success_metric',
-           y_range=(0,20),
+           y_range=(0,15),
            tools=['crosshair,resize,reset,save'])
 
-p.circle('2016_02', 'success_metric', legend="All Stores Usage", color='lightblue',alpha=0.4, source=source)
+p.circle('2016_02', 'success_metric', legend="success_metric", color='lightblue',alpha=0.4, source=source)
 
 
 output_file("success_vs_housing.html", title="Success vs Housing cost")
