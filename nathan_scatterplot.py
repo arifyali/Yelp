09d4d03c6a24b9cdd6d8d670e430e24f3b07d6ce
Nathan <hadoop@ip-172-31-60-151.ec2.internal>
2016-05-01 00:33:41 +0000

    scatter plot

--- nathan_scatterplot.py
+++ nathan_scatterplot.py
@@ -7,14 +7,18 @@ from pyspark.mllib.linalg import Vectors
 from pyspark.mllib.evaluation import RegressionMetrics
 import matplotlib.pyplot as plt
 
-sc = SparkContext()
+#sc = SparkContext()
 sqlContext = HiveContext(sc)
 # The races from the census data were normalized in order
 qry = "SELECT AVG(success_metric) as success_metric, 2016_02, zipcode from census_rest_success group by zipcode, 2016_02"
 
 df = sqlContext.sql(qry)
 
-x = df["2016_02"]
-y = df["
+x = df.select("2016_02").collect()
+y = df.select("success_metric").collect()
 
 plt.scatter(x, y)
+
+plt.show()
+
+plt.savefig("plot.png", format='png')
