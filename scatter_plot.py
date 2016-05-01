0abbccfa23a91e196231292d4363a6e42e696ccd
Jordan Bramble <jordanbramble@gmail.com>
2016-04-30 20:30:09 -0400

    adds scatter plot of success_metric vs housing cost

---
+++ scatter_plot.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+from bokeh.plotting import Figure, show, output_file, ColumnDataSource
+
+df = pd.read_csv('success_vs_housing.csv')
+print df.head()
+
+source = ColumnDataSource(data=df)
+
+p = Figure(title='Success Metric vs Housing Costs',
+           x_axis_label='$/sq. ft.',
+           y_axis_label='success_metric',
+           y_range=20,
+           tools=['crosshair,resize,reset,save'])
+
+p.circle('2016_02', 'success_metric', legend="All Stores Usage", color='lightblue',alpha=0.4, source=source)
+
+
+output_file("success_vs_housing.html", title="Success vs Housing cost")
+show(p)
